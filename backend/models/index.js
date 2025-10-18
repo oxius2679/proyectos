1d5cfd5080ccc2139c274747a5752d99e1d290c4
Angel <ajackson2672@gmail.com>
2025-10-19 00:55:12 +0200

    Agregar modelo User con conexión a MongoDB

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,12 @@
-// Ejemplo mínimo para que no falle
+const { MongoClient } = require('mongodb');
+
+// Usa la misma conexión que en server.js
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db('gestion_proyectos');
+
 const User = {
-  findOne: async () => null,
-  findById: async () => null
+  findOne: async (filter) => await db.collection('users').findOne(filter),
+  findById: async (id) => await db.collection('users').findOne({ _id: id })
 };
 
 module.exports = { User };
