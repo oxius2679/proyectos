b12cc978f1736e99c5fd67f568acdcd1c1142b65
Angel <ajackson2672@gmail.com>
2025-10-19 04:14:00 +0200

    ter modificacion

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); // 👈 ¡Esta línea es obligatoria!
+
 
 // Recibe el cliente como parámetro
 module.exports = (client) => {
